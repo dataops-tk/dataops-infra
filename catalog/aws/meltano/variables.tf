ecedb3da2c3f969dd11b586f21b319693fb4a443
AJ Steers <aaronsteers@gmail.com>
2021-03-30 11:25:28 -0700

    begin update module variables

--- catalog/aws/meltano/variables.tf
+++ catalog/aws/meltano/variables.tf
@@ -46,52 +46,14 @@ EOF
     settings = map(string)
     secrets  = map(string)
   }))
-  # type        = list(map(any))
-  # validation {
-  #   condition = length([
-  #     for tap in var.taps :
-  #     tap["id"]
-  #     if length(setintersect(keys(tap), ["id", "settings", "secrets"])) < 3 # missing required key
-  #   ]) > 0
-  #   error_message = "One or more tap configurations is missing a required key. Expected minimum config: 'id', 'settings', and 'secrets'."
-  # }
-  # validation {
-  #   condition = length([
-  #     for tap in var.taps :
-  #     tap["id"]
-  #     if length(setsubtract(keys(tap), ["id", "settings", "secrets", "name", "schedule"])) > 0 # unknown keys
-  #   ]) > 0
-  #   error_message = "One or more tap configurations has an unexpected key. Allowed keys: 'id', 'name', 'schedule', 'settings', and 'secrets'."
-  # }
-}
-variable "target" {
-  description = <<EOF
-The definition of which target to load data into.
-Note: You must specify `target` or `data_lake_storage_path` but not both.
-See the 'taps' input variable for more information on expected configuration values.
-EOF
-  type = object({
-    id       = string
-    settings = map(string)
-    secrets  = map(string)
-  })
-  default = null
-}
-variable "pipeline_version_number" {
-  description = <<EOF
-Optional. (Default="1") Specify a pipeline version number when there are breaking changes which require
-isolation. Note if you want to avoid overlap between versions, be sure to (1) cancel the
-previous version and (2) specify a `start_date` on the new version which is not duplicative
-of the previously covered time period.
-EOF
-  type        = string
-  default     = "1"
 }
 
-# Local Config Files
-
-variable "local_metadata_path" {
-  description = "The local folder which countains tap definitions files: `{tap-name}.rules.txt` and `{tap-name}.plan.yml`"
+variable "project_source" {
+  description = <<EOF
+The source repo which contains your project source.
+If provided as a local reference (without 'http' in the string) the project will be locally linked.
+Otherwise, a git-based connection to the repo will be attempted.
+EOF
   type        = string
 }
 
@@ -99,7 +61,7 @@ variable "local_metadata_path" {
 
 variable "data_lake_type" {
   description = "Specify `S3` if loading to an S3 data lake, otherwise leave blank."
-  default     = null
+  default     = "S3"
 }
 variable "data_lake_logging_path" {
   description = <<EOF
@@ -130,15 +92,8 @@ EOF
 
 # Orchestration
 
-variable "scheduled_timezone" {
-  description = <<EOF
-The timezone used in scheduling.
-Currently the following codes are supported: PST, PDT, EST, UTC
-EOF
-  default     = "PST"
-}
 variable "timeout_hours" {
-  description = "Optional. The number of hours before the sync task is canceled and retried."
+  description = "Optional. The number of hours before a sync task is canceled and retried."
   type        = number
   default     = 48
 }
@@ -149,14 +104,24 @@ variable "num_retries" {
 
 # Environment Config
 
-variable "container_num_cores" {
+variable "ui_container_num_cores" {
   description = "Optional. Specify the number of cores to use in the container."
   default     = 0.5
 }
-variable "container_ram_gb" {
+variable "ui_container_ram_gb" {
   description = "Optional. Specify the amount of RAM to be available to the container."
   default     = 1
 }
+
+variable "elt_container_num_cores" {
+  description = "Optional. Specify the number of cores to use in the container."
+  default     = 0.5
+}
+variable "elt_container_ram_gb" {
+  description = "Optional. Specify the amount of RAM to be available to the container."
+  default     = 1
+}
+
 variable "use_private_subnet" {
   description = <<EOF
 If True, tasks will use a private subnet and will require a NAT gateway to pull the docker
